07146246e5009545e3216bb85724e08c8f597788
motohiro84 <motogen.8040-1995@docomo.ne.jp>
2020-11-04 11:21:57 +0900

    マズルフラッシュ

---
+++ Assets/Scripts/FirstPersonGunController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FirstPersonGunController : MonoBehaviour
+{
+  public bool shootEnabled = true;
+
+  public int maxAmmo = 6;
+
+  private float shootInterval = 0.15f;
+  private float shootRange = 50;
+  public Vector3 muzzleFlashScale;
+  public GameObject muzzleFlashPrefab;
+  public GameObject hitEffectPrefab;
+  bool shooting = false;
+  int ammo;
+  GameObject muzzleFlash;
+  GameObject hitEffect;
+  public int Ammo
+  {
+    set
+    {
+      ammo = Mathf.Clamp(value, 0, maxAmmo);
+    }
+    get
+    {
+      return ammo;
+    }
+  }
+  void Start()
+  {
+    InitGun();
+  }
+  void Update()
+  {
+    if (shootEnabled & ammo > 0 & GetInput())
+    {
+      StartCoroutine(ShootTimer());
+    }
+  }
+  void InitGun()
+  {
+    Ammo = maxAmmo;
+  }
+  bool GetInput()
+  {
+
+    return Input.GetMouseButtonDown(0);
+    return false;
+  }
+  IEnumerator ShootTimer()
+  {
+    if (!shooting)
+    {
+      shooting = true;
+      //マズルフラッシュON
+      if (muzzleFlashPrefab != null)
+      {
+        if (muzzleFlash != null)
+        {
+          muzzleFlash.SetActive(true);
+        }
+        else
+        {
+          muzzleFlash = Instantiate(muzzleFlashPrefab, transform.position, transform.rotation);
+          muzzleFlash.transform.SetParent(gameObject.transform);
+          muzzleFlash.transform.localScale = muzzleFlashScale;
+        }
+      }
+      Shoot();
+      yield return new WaitForSeconds(shootInterval);
+      //マズルフラッシュOFF
+      if (muzzleFlash != null)
+      {
+        muzzleFlash.SetActive(false);
+      }
+      //ヒットエフェクトOFF
+      if (hitEffect != null)
+      {
+        if (hitEffect.activeSelf)
+        {
+          hitEffect.SetActive(false);
+        }
+      }
+      shooting = false;
+    }
+    else
+    {
+      yield return null;
+    }
+  }
+  void Shoot()
+  {
+    Ray ray = new Ray(transform.position, transform.forward);
+    RaycastHit hit;
+    //レイを飛ばして、ヒットしたオブジェクトの情報を得る
+    if (Physics.Raycast(ray, out hit, shootRange))
+    {
+      //ヒットエフェクトON
+      if (hitEffectPrefab != null)
+      {
+        if (hitEffect != null)
+        {
+          hitEffect.transform.position = hit.point;
+          hitEffect.transform.rotation = Quaternion.FromToRotation(Vector3.forward, hit.normal);
+          hitEffect.SetActive(true);
+        }
+        else
+        {
+          hitEffect = Instantiate(hitEffectPrefab, hit.point, Quaternion.identity);
+        }
+      }
+      //★ここに敵へのダメージ処理などを追加
+    }
+    Ammo--;
+  }
+}

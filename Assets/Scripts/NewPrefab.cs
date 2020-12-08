237e2c51ad0e54c2f75f8e6aabb1902381978842
motohiro84 <motogen.8040-1995@docomo.ne.jp>
2020-12-08 16:30:05 +0900

    ミュージックエンジン

---
+++ Assets/Scripts/NewPrefab.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewPrefab : MonoBehaviour
+{
+  bool key = true;
+  public GameObject prefab1;
+  public GameObject obj;
+
+  float n = 0;
+  float a;
+  float b;
+  float ave;
+  float plus;
+
+  void Start()
+  {
+  }
+
+  void Update()
+  {
+    if (Music.IsJustChangedBar() && Time.time > 4)
+    {
+      PrefabNew();
+      a = Time.time;
+      if (n == 0)
+      {
+        b = Time.time;
+        n++;
+      }
+      else
+      {
+        plus += (a - b);
+        ave = plus / n;
+        n++;
+        b = Time.time;
+      }
+      Debug.Log(ave);
+    }
+  }
+
+  void PrefabNew()
+  {
+    if (key)
+    {
+      prefab1.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+      key = false;
+    }
+    else
+    {
+      prefab1.transform.localScale = new Vector3(1, 1, 1);
+      key = true;
+    }
+    GameObject clone = Instantiate(prefab1, obj.transform);
+    clone.transform.SetParent(obj.transform, false);
+  }
+
+}
